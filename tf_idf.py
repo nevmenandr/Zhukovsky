5c0da4eed2bf59d36bf3a9c583866c7c49052fde
ElizavetaKuzmenko <lizaku77@gmail.com>
2015-10-04 02:27:30 +0300

    more functions added

--- tf_idf.py
+++ tf_idf.py
@@ -22,10 +22,10 @@ def compute_tf(text):
     #возвращаем объект типа Counter c TF всех слов текста
     return tf_text
 
-text = ['hasta', 'la', 'vista', 'baby', 'la', 'vista', 'la']
-print compute_tf(text)
-Out: Counter({'la': 0.42857142857142855, 'vista': 0.2857142857142857, 
-'hasta': 0.14285714285714285, 'baby': 0.14285714285714285})
+#text = ['hasta', 'la', 'vista', 'baby', 'la', 'vista', 'la']
+#print compute_tf(text)
+#Out: Counter({'la': 0.42857142857142855, 'vista': 0.2857142857142857,
+#'hasta': 0.14285714285714285, 'baby': 0.14285714285714285})
 
 def compute_idf(word, corpus):
 #на вход берется слово, для которого считаем IDF
@@ -34,49 +34,40 @@ def compute_idf(word, corpus):
         #считается как генератор списков
         return math.log10(len(corpus)/sum([1.0 for i in corpus if word in i]))
         
-texts = [['pasta', 'la', 'vista', 'baby', 'la', 'vista'], 
-['hasta', 'siempre', 'comandante', 'baby', 'la', 'siempre'], 
-['siempre', 'comandante', 'baby', 'la', 'siempre']]
-print compute_idf('pasta', texts)
-Out: 0.47712125472
+#texts = [['pasta', 'la', 'vista', 'baby', 'la', 'vista'],
+#['hasta', 'siempre', 'comandante', 'baby', 'la', 'siempre'],
+#['siempre', 'comandante', 'baby', 'la', 'siempre']]
+#print compute_idf('pasta', texts)
+#Out: 0.47712125472
 
 ############################################################
 # the same:
 
-def compute_tfidf(corpus):
-    def compute_tf(text):
-        tf_text = Counter(text)
-        for i in tf_text:
-            tf_text[i] = tf_text[i]/float(len(tf_text))
-        return tf_text
+def compute_tfidf(line, corpus):
+    #documents_list = []
+    #for text in corpus:
+    tf_idf_dictionary = {}
+    computed_tf = compute_tf(line)
+    for word in computed_tf:
+        tf_idf_dictionary[word] = computed_tf[word] * compute_idf(word, corpus)
+    #    documents_list.append(tf_idf_dictionary)
+    return tf_idf_dictionary
 
-    def compute_idf(word, corpus):
-        return math.log10(len(corpus)/sum([1.0 for i in corpus if word in i]))
+#corpus = [['pasta', 'la', 'vista', 'baby', 'la', 'vista'],
+#['hasta', 'siempre', 'comandante', 'baby', 'la', 'siempre'],
+#['siempre', 'comandante', 'baby', 'la', 'siempre']]
+#print compute_tfidf(corpus)
+#Out: [{'pasta': 0.11928031367991561, 'baby': 0.0, 'vista': 0.23856062735983122, 'la': 0.0},
+#'hasta': 0.09542425094393249, 'comandante': 0.03521825181113625, 'siempre': 0.0704365036222725,
+#'baby': 0.0, 'la': 0.0},
+#{'comandante': 0.04402281476392031, 'baby': 0.0, 'siempre': 0.08804562952784062, 'la': 0.0}]
 
-    documents_list = []
-    for text in corpus:
-        tf_idf_dictionary = {}
-        computed_tf = compute_tf(text)
-        for word in computed_tf:
-            tf_idf_dictionary[word] = computed_tf[word] * compute_idf(word, corpus)
-        documents_list.append(tf_idf_dictionary)
-    return documents_list
-
-corpus = [['pasta', 'la', 'vista', 'baby', 'la', 'vista'], 
-['hasta', 'siempre', 'comandante', 'baby', 'la', 'siempre'], 
-['siempre', 'comandante', 'baby', 'la', 'siempre']]
-print compute_tfidf(corpus)
-Out: [{'pasta': 0.11928031367991561, 'baby': 0.0, 'vista': 0.23856062735983122, 'la': 0.0}, 
-{'hasta': 0.09542425094393249, 'comandante': 0.03521825181113625, 'siempre': 0.0704365036222725, 
-'baby': 0.0, 'la': 0.0}, 
-{'comandante': 0.04402281476392031, 'baby': 0.0, 'siempre': 0.08804562952784062, 'la': 0.0}]
-
-def main():
+#def main():
     
-    return 0
+#    return 0
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+#    main()
 
 #from sklearn.feature_extraction.text import TfidfVectorizer
 #corpus = ["This is very strange",
